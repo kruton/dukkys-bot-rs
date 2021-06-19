82cafe1fd0045468b417825e80c9853c818e4e81
Kenny Root <kenny@the-b.org>
2021-06-18 23:32:31 -0700

    Add client connection

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 extern crate dotenv;
 
+use std::env;
 use dotenv::dotenv;
+
+use serenity::prelude::*;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
+    let token = env::var("TOKEN")
+        .expect("Expected TOKEN environment");
+
+    let mut client = Client::builder(&token)
+        .await
+        .expect("Error creating Discord client");
+
+    if let Err(why) = client.start().await {
+        eprintln!("Client error: {:?}", why);
+    }
 }
