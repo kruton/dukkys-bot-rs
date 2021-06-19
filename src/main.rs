aac9112a8a5456bc2c12e3428f4e616086acdef5
Kenny Root <kenny@the-b.org>
2021-06-18 23:56:23 -0700

    Add command framework and help command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod commands;
+
 extern crate dotenv;
 
 use std::env;
@@ -5,26 +7,28 @@ use dotenv::dotenv;
 
 use serenity::{
     async_trait,
-    model::{channel::Message, gateway::Ready},
+    framework::{
+        StandardFramework,
+        standard::macros::group,
+    },
+    model::gateway::Ready,
     prelude::*
 };
+
+use commands::help::*;
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {:?}", why);
-            }
-        }
-    }
-
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
 }
+
+#[group]
+#[commands(help)]
+struct General;
 
 #[tokio::main]
 async fn main() {
@@ -33,7 +37,12 @@ async fn main() {
     let token = env::var("TOKEN")
         .expect("Expected TOKEN environment");
 
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("-"))
+        .group(&GENERAL_GROUP);
+
     let mut client = Client::builder(&token)
+        .framework(framework)
         .event_handler(Handler)
         .await
         .expect("Error creating Discord client");
