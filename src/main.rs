4a5be003de66a3d212b252dda0e255bbb258494f
Kenny Root <kenny@the-b.org>
2021-06-18 23:37:37 -0700

    Add ping command

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use dotenv::dotenv;
 
 use serenity::{
     async_trait,
-    model::gateway::Ready,
+    model::{channel::Message, gateway::Ready},
     prelude::*
 };
 
@@ -13,6 +13,14 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
