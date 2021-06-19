35cb65c0ca828d868b35556d2c2dac8394e29898
Kenny Root <kenny@the-b.org>
2021-06-19 00:09:03 -0700

    Add message formatting

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -1,13 +1,24 @@
-use serenity::prelude::*;
-use serenity::model::prelude::*;
-use serenity::framework::standard::{
-    CommandResult,
-    macros::command,
+use serenity::{
+    prelude::*,
+    model::prelude::*,
+    framework::standard::{
+        CommandResult,
+        macros::command,
+    },
+    utils::MessageBuilder,
 };
 
 #[command]
 async fn help(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.channel_id.say(&ctx.http, "You have been helped!").await?;
+    let response = MessageBuilder::new()
+        .push("Hi, ")
+        .push_bold_safe(&msg.author.name)
+        .push(". thanks.")
+        .build();
+
+        if let Err(why) = msg.channel_id.say(&ctx.http, &response).await {
+            println!("Erorr sending message: {:?}", why);
+        }
 
     Ok(())
 }
