e8402969a1145fe195912daeb8c25b89a1155a77
Kenny Root <kenny@the-b.org>
2021-06-21 20:21:28 -0700

    Handle Ctrl-C correctly for Docker

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,15 @@ async fn main() {
         .await
         .expect("Error creating Discord client");
 
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Could not register ctrl+c handler");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+
     if let Err(why) = client.start().await {
         eprintln!("Client error: {:?}", why);
     }
